f71267f93303d01a2d235c7f73a28bba7f394f80
eduardadefiume-homy <eduarda.defiume@homyquimica.com.br>
2026-05-28 14:39:45 -0300

    Add files via upload

---
+++ admin/js/admin-dashboard.js
@@ -0,0 +1,9 @@
+// ============================================================
+// admin-dashboard.js — Dashboard administrativo
+// ============================================================
+
+window.AdminDashboard = {
+  async carregar() {
+    console.log("Carregar dashboard.");
+  }
+};
