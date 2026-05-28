0aa0330e6e7f11b5c36a3af1168ff748f52dca33
eduardadefiume-homy <eduarda.defiume@homyquimica.com.br>
2026-05-28 14:38:13 -0300

    Add files via upload

---
+++ admin/js/admin-cardapio.js
@@ -0,0 +1,18 @@
+// ============================================================
+// admin-cardapio.js — CRUD do cardápio
+// Principal, Light, Carne, Massa e Lanche
+// ============================================================
+
+window.AdminCardapio = {
+  async carregar() {
+    console.log("Carregar cardápio.");
+  },
+
+  async editar(id) {
+    console.log("Editar cardápio", id);
+  },
+
+  async excluir(id) {
+    console.log("Excluir cardápio", id);
+  }
+};
