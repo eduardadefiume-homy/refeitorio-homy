deff477f48e6f6168fef53a69e2fbd5d6f3aa3bb
eduardadefiume-homy <eduarda.defiume@homyquimica.com.br>
2026-05-28 14:40:02 -0300

    Add files via upload

---
+++ admin/js/admin-extras.js
@@ -0,0 +1,17 @@
+// ============================================================
+// admin-extras.js — Refeições extras
+// ============================================================
+
+window.AdminExtras = {
+  async carregar() {
+    console.log("Carregar extras.");
+  },
+
+  async editar(id) {
+    console.log("Editar extra", id);
+  },
+
+  async excluir(id) {
+    console.log("Excluir extra", id);
+  }
+};
